74657525b09d2ebe21194f446f02df7586b2c9b5
Jacqueline Tropp <jacquelinetropp@gmail.com>
2021-03-04 21:30:25 -0600

    dfa site fix

--- src/pages/CovidVaccine.js
+++ src/pages/CovidVaccine.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 
 import PortfolioProject from "../components/portfoliopage/portfolioPage";
 
@@ -12,13 +12,14 @@ const CovidVaccine = () => {
     window.scrollTo(0, 0);
   });
   return (
+    <Fragment>
     <PortfolioProject
       headerImg={headerImg}
       title="DFA Covid Vaccine Toolkit"
       description="A toolkit for the latest Covid-19 vaccine information built for Doctors For America"
       techs={[jquery, wordpress, sass]}
-      github="https://github.com/jacquelinetropp/ultimateplanner2"
-      demo="https://ultimateplanner.herokuapp.com/"
+      github="https://github.com/jacquelinetropp/vaccinetoolkit"
+      demo="https://www.drsforamerica.org/covid-vaccine-toolkit/"
       highlight1_title="About The Website"
       highlight1_text="This website was Doctors For America, a non-profit organization. The organization was in the middle of a rebrand and updating
       their main website. They needed a website built as soon as possible to display the covid vaccine information. I built the website originally 
@@ -28,6 +29,8 @@ const CovidVaccine = () => {
       mind, I used client-friendly pluggins to allow the client to quickly go in and edit information on the website without the help of a web developer.
       I sent step-by-step tutorials to ensure the client had everything they needed to easily manage the site on their own."
     />
+    <p className="ogdesign">To see the original design, click <a href="https://jacquelinetropp.github.io/dfahtml/">Here</a></p>
+    </Fragment>
   );
 };
 
