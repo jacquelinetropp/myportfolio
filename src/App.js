ff6e92471c829bc2f46b2604048ed56afb11f5b4
Jacqueline Tropp <jacquelinetropp@gmail.com>
2021-03-07 15:48:57 -0600

    suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Header from "./components/header/header";
@@ -6,44 +6,34 @@ import Header from "./components/header/header";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
+
 import ContactForm from "./components/contact/contact";
-import BackpackingBuddies from "./pages/BackpackingBuddies";
+
+
 import UltimatePlanner from "./pages/UltimatePlanner";
-import CovidVaccine from "./pages/CovidVaccine";
-import OnlineTraining from "./pages/OnlineTraining";
-import PersonalBlog from "./pages/PersonalBlog";
+const CovidVaccine = React.lazy(() =>  import("./pages/CovidVaccine"));
+const OnlineTraining = React.lazy(() =>  import("./pages/OnlineTraining"));
+const PersonalBlog = React.lazy(() =>  import("./pages/PersonalBlog"));
+const BackpackingBuddies = React.lazy(() => import("./pages/BackpackingBuddies"));
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route
-          exact
-          path="/backpacking-buddies"
-          component={BackpackingBuddies}
-        />
-        <Route
-          exact
-          path="/ultimate-planner"
-          component={UltimatePlanner}
-        />
-        <Route
-        exact
-        path="/vaccine-toolkit"
-        component={CovidVaccine}
-      />
-      <Route
-      exact
-      path="/online-training"
-      component={OnlineTraining}
-    />
-    <Route
-    exact
-    path="/personal-blog"
-    component={PersonalBlog}
-  />
+        <Suspense>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/backpacking-buddies"
+            component={BackpackingBuddies}
+          />
+          <Route exact path="/ultimate-planner" component={UltimatePlanner} />
+          <Route exact path="/vaccine-toolkit" component={CovidVaccine} />
+          <Route exact path="/online-training" component={OnlineTraining} />
+          <Route exact path="/personal-blog" component={PersonalBlog} />
+        </Suspense>
+        
       </Switch>
       <ContactForm />
     </Fragment>
